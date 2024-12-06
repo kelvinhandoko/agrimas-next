0eb2b0db30422d7816c75c78b99130834dfbf27c
--replace-all <kelvinhandoko03@gmail.com>
2024-12-06 07:31:49 +0700

    feat: sub account model

---
+++ src/server/subAccount/sub-account.model.ts
@@ -0,0 +1,36 @@
+import { basicQuery } from "@/server/common/models/basic";
+import { type Prisma } from "@prisma/client";
+import { z } from "zod";
+
+type SubAccountInclude<T> = {
+  include?: Prisma.Subset<T, Prisma.SubAccountInclude>;
+};
+
+export const subAccountPayloadSchema = z.object({
+  name: z
+    .string({ required_error: "nama akun tidak boleh kosong" })
+    .min(1, "nama akun minimal 1 huruf."),
+  companyId: z.string({ required_error: "perusahaan tidak boleh kosong" }),
+  accountId: z.string({
+    required_error: "nama akun tidak boleh kosong",
+  }),
+  id: z.string().optional(),
+});
+
+export type SubAccountPayload = z.infer<typeof subAccountPayloadSchema>;
+
+export const getAllSubAccountQuerySchema = basicQuery;
+
+export type GetAllSubAccountQuery<T> = z.infer<
+  typeof getAllSubAccountQuerySchema
+> &
+  SubAccountInclude<T>;
+
+export const getDetailSubAccountQuerySchema = z.object({
+  id: z.string(),
+});
+
+export type GetDetailSubAccountQuery<T> = z.infer<
+  typeof getDetailSubAccountQuerySchema
+> &
+  SubAccountInclude<T>;
