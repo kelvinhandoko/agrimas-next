d19f81a647f23d9ceb0d43f7aa8f5be3bf4a3eaf
--replace-all <kelvinhandoko03@gmail.com>
2025-01-06 14:18:26 +0700

    refactor: Edit Product page to use a Promise for params type

--- src/app/(private-route)/(main-layout)/data-master/product/[id]/edit/page.tsx
+++ src/app/(private-route)/(main-layout)/data-master/product/[id]/edit/page.tsx
@@ -1,13 +1,16 @@
 import { type Metadata } from "next";
 
+type Params = Promise<{ id: string }>;
+
 export const metadata: Metadata = {
   title: "Agrimasi - Edit Product Page",
   description:
     "Select a company to proceed based on your role. Streamline navigation and access specific company data with ease.",
   icons: [{ rel: "icon", url: "/favicon.ico" }],
 };
-const page = ({ params }: { params: { id: string } }) => {
-  return <div>Edit Product page {params.id}</div>;
+const page = async ({ params }: { params: Params }) => {
+  const id = (await params).id;
+  return <div>Edit Product page {id}</div>;
 };
 
 export default page;
