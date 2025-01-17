249bf186a905e97eba4e928f98412e7c1f0f8297
--replace-all <kelvinhandoko03@gmail.com>
2025-01-17 23:36:33 +0700

    lint: add eslint rule

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -35,7 +35,16 @@ const config = {
         },
       },
     ],
-    "unused-imports/no-unused-vars": "warn",
+    "unused-imports/no-unused-imports": "error",
+    "unused-imports/no-unused-vars": [
+      "warn",
+      {
+        vars: "all",
+        varsIgnorePattern: "^_",
+        args: "after-used",
+        argsIgnorePattern: "^_",
+      },
+    ],
   },
 };
 module.exports = config;
