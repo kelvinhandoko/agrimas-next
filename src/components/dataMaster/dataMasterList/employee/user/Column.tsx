7899032b06cf4f0d4b1c5a4d1ce54738be6a67e8
IrvanYusuf <irvanyusufcahyadi4@gmail.com>
2025-02-17 20:33:04 +0700

    error delete user

--- src/components/dataMaster/dataMasterList/employee/user/Column.tsx
+++ src/components/dataMaster/dataMasterList/employee/user/Column.tsx
@@ -1,46 +1,53 @@
 import { paths } from "@/paths/paths";
 import { Flex } from "@radix-ui/themes";
 import { type ColumnDef, createColumnHelper } from "@tanstack/react-table";
-import { Eye, PencilIcon } from "lucide-react";
+import { PencilIcon } from "lucide-react";
 import Link from "next/link";
 
-import { type CustomerRouterOutputs } from "@/server/customer";
+import { type UserRouterOutputs } from "@/server/user/user.router";
 
 import DeleteModal from "@/components/DeleteModal";
 
-import DetailCustomerModal from "../../customer/detailCustomerModal";
+import DetailUserModal from "./DetailUserModal";
 
 const columnHelper =
-  createColumnHelper<CustomerRouterOutputs["getAll"]["data"][0]>();
+  createColumnHelper<UserRouterOutputs["getAll"]["data"][0]>();
 
-export const customerlColumn = [
-  columnHelper.accessor("nama", {
-    header: () => <div>Nama</div>,
-    cell: ({ row }) => <div className="lowercase">{row.getValue("nama")}</div>,
-  }),
-  columnHelper.accessor("alamat", {
-    header: () => <div>Alamat</div>,
-    cell: (info) => info.getValue(),
-  }),
-  {
-    id: "actions",
-    enableHiding: false,
-    header: () => <div className="text-center">Aksi</div>,
-    cell: ({ row }) => (
-      <Flex justify="center" gapX="3">
-        <DetailCustomerModal
-          id={row.original.id}
-          name={row.original.nama}
-          alamat={row?.original?.alamat ?? ""}
-        />
-        <Link
-          href={paths.dataMaster.customer.edit(row.original.id)}
-          className="text-yellow-400"
-        >
-          <PencilIcon className="text-yellow-400" />
-        </Link>
-        <DeleteModal id={row.original.id} name={row.original.nama} />
-      </Flex>
-    ),
-  },
-] as ColumnDef<CustomerRouterOutputs["getAll"]["data"][0]>[];
+export const userlColumn = (handleDeleteUser: (id: string) => void) =>
+  [
+    columnHelper.accessor("username", {
+      header: () => <div>Nama</div>,
+      cell: ({ row }) => (
+        <div className="lowercase">{row.getValue("username")}</div>
+      ),
+    }),
+    columnHelper.accessor("role", {
+      header: () => <div>Role</div>,
+      cell: (info) => info.getValue(),
+    }),
+    {
+      id: "actions",
+      enableHiding: false,
+      header: () => <div className="text-center">Aksi</div>,
+      cell: ({ row }) => (
+        <Flex justify="center" gapX="3">
+          <DetailUserModal
+            id={row.original.id}
+            name={row.original.username}
+            role={row.original.role}
+          />
+          <Link
+            href={paths.dataMaster.employee.editUser(row.original.id)}
+            className="text-yellow-400"
+          >
+            <PencilIcon className="text-yellow-400" />
+          </Link>
+          <DeleteModal
+            id={row.original.id}
+            name={row.original.username}
+            handleDelete={() => handleDeleteUser(row.original.id)}
+          />
+        </Flex>
+      ),
+    },
+  ] as ColumnDef<UserRouterOutputs["getAll"]["data"][0]>[];
--- src/components/dataMaster/dataMasterList/employee/user/UserDataTable.tsx
+++ src/components/dataMaster/dataMasterList/employee/user/UserDataTable.tsx
@@ -2,23 +2,42 @@
 
 import { paths } from "@/paths/paths";
 import { api } from "@/trpc/react";
-import { Box } from "@radix-ui/themes";
+import { toast } from "sonner";
 
 import LoadingIndicator from "@/components/LoadingIndicator";
 import DataTable from "@/components/common/table/DataTable";
 
-import { customerlColumn } from "../../customer/Column";
+import { userlColumn } from "./Column";
 
 const UserDataTable = () => {
-  const { data, isLoading } = api.supplier.getAll.useQuery({});
+  const utils = api.useUtils();
+  const { data, isLoading } = api.user.getAll.useQuery({});
+
+  const { mutateAsync: deleteUser } = api.user.delete.useMutation({
+    onSuccess: async () => {
+      toast.success("Berhasil hapus user");
+      await utils.user.getAll.invalidate();
+    },
+    onError: () => {
+      toast.error("Gagal menghapus user");
+    },
+  });
+
+  const handleDeleteUser = async (id: string) => {
+    try {
+      await deleteUser(id);
+    } catch (error) {
+      console.error("Error deleting user:", error);
+    }
+  };
   if (isLoading) {
     return <LoadingIndicator />;
   }
   return (
     <DataTable
-      columns={customerlColumn}
+      columns={userlColumn(handleDeleteUser)}
       data={data?.data || []}
-      colFilterName="nama"
+      colFilterName="username"
       path={paths.dataMaster.employee.newUser}
       buttonAddName="Tambah User"
     />
