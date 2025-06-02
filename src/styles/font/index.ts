2335776198a0e8710ac59a74ee97fd8ebe1accee
kelvin <kelvinhandoko03@gmail.com>
2025-06-02 16:56:17 +0700

    refix

--- src/styles/font/index.ts
+++ src/styles/font/index.ts
@@ -1,4 +1,6 @@
 import "next/font/google";
 import { Plus_Jakarta_Sans } from "next/font/google";
 
-export const jakartaSans = Plus_Jakarta_Sans();
+export const jakartaSans = Plus_Jakarta_Sans({
+  subsets: ["latin"],
+});
