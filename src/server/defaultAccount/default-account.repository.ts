5367ef1da2583be9193e38693bb1cd4a0810a303
kelvin <kelvinhandoko03@gmail.com>
2025-06-02 12:20:32 +0700

    fix default accounting data

--- src/server/defaultAccount/default-account.repository.ts
+++ src/server/defaultAccount/default-account.repository.ts
@@ -1,4 +1,6 @@
 import { type DefaultAccountPayload } from "@/model/default-account.model";
+import { TRANSACTION_CATEGORY } from "@prisma/client";
+import { TRPCError } from "@trpc/server";
 
 import { BaseRepository } from "@/server/common";
 
@@ -11,8 +13,30 @@ export class DefaultAccountRepository extends BaseRepository {
   }
 
   async getByCompany(companyId: string) {
-    return await this._db.defaultAccount.findMany({
+    const data = await this._db.defaultAccount.findMany({
       where: { companyId },
+      select: { category: true, id: true },
     });
+
+    const map = new Map<keyof typeof TRANSACTION_CATEGORY, string>();
+
+    for (const record of data) {
+      if (record.category) {
+        map.set(record.category, record.id);
+      }
+    }
+
+    const missing = Object.values(TRANSACTION_CATEGORY).filter(
+      (category) => !map.has(category),
+    );
+
+    if (missing.length > 0) {
+      throw new TRPCError({
+        code: "BAD_REQUEST",
+        message: `Missing default account for ${missing.join(", ")}`,
+      });
+    }
+
+    return map;
   }
 }
