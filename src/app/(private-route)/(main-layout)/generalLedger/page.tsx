9c6633bd750fbda193cbba4626dc64b47f37b6b6
kelvin <kelvinhandoko03@gmail.com>
2025-06-03 09:46:52 +0700

    refactor: remove PurchaseOrderRow and PurchaseOrderSummaryForm components
    refactor: delete unused PurchasePayment components and related files
    
    refactor: clean up create-product.controller by removing unused dependencies and logic

--- src/app/(private-route)/(main-layout)/generalLedger/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-import GeneralLedgerList from "@/components/generalLedger/GeneralLedgerList";
-
-const page = () => {
-  return <GeneralLedgerList />;
-};
-
-export default page;
