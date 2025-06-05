fb523aaf1d6da8427ceb4b1eae558749cd0c65c1
--replace-all <kelvinhandoko03@gmail.com>
2025-06-05 18:56:01 +0700

    feat: implement JournalForm component with date input and submission logic

--- src/components/accountant/journal/form/index.tsx
+++ src/components/accountant/journal/form/index.tsx
@@ -1,17 +1,69 @@
 "use client";
 
 import { type FORM_TYPE } from "@/constant";
+import { type JournalPayload, journalPayloadSchema } from "@/model";
+import { paths } from "@/paths/paths";
 import { api } from "@/trpc/react";
+import { errorFormatter } from "@/utils/formatter/errorFormatter";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useRouter } from "next/navigation";
 import { type FC } from "react";
+import { type SubmitHandler, useForm } from "react-hook-form";
+import { toast } from "sonner";
+
+import BackButton from "@/components/BackButton";
+import DateInput from "@/components/accountant/journal/form/DateInput";
+import { Card, CardContent, CardHeader } from "@/components/ui/card";
+import { Form } from "@/components/ui/form";
 
 interface JournalFormProps {
   type?: FORM_TYPE;
 }
 
+const today = new Date();
+
 const JournalForm: FC<JournalFormProps> = ({ type = "CREATE" }) => {
   const utils = api.useUtils();
-  const { mutateAsync } = api.journal.create.useMutation();
-  return <div>JournalForm</div>;
+  const router = useRouter();
+  const { mutateAsync: createJournal } = api.journal.create.useMutation();
+  const form = useForm<JournalPayload>({
+    resolver: zodResolver(journalPayloadSchema),
+    defaultValues: {
+      date: today,
+    },
+  });
+
+  const onSubmit: SubmitHandler<JournalPayload> = async (data) => {
+    toast.promise(
+      async () => {
+        await createJournal(data);
+      },
+      {
+        loading: "Loading...",
+        success: async () => {
+          router.push("/accountant/journal");
+          await utils.journal.get.invalidate();
+          return "berhasil membuat jurnal umum";
+        },
+        error: errorFormatter,
+      },
+    );
+  };
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} autoComplete="off">
+        <Card>
+          <CardHeader>
+            <BackButton path={paths.accountant.journal} />
+          </CardHeader>
+          <CardContent>
+            <DateInput form={form} />
+          </CardContent>
+        </Card>
+      </form>
+    </Form>
+  );
 };
 
 export default JournalForm;
