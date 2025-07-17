53b8c463222182f9050046fa2e8d5f7ba865f69b
irvan <irvanyusufcahyadi4@gmail.com>
2025-07-17 23:23:14 +0700

    feat: create Payment Method page and update Receiveable page to use ReceiveablePage component

--- src/app/(main-layout)/finance/payment-method/page.tsx
+++ src/app/(main-layout)/finance/payment-method/page.tsx
@@ -1,15 +1,15 @@
+import { HydrateClient } from "@/trpc/server";
 import { type Metadata } from "next";
-
-import ReceiveablePage from "@/components/report/receiveable/ReceiveablePage";
+import React from "react";
 
 export const metadata: Metadata = {
-  title: "Agrimas - Report Receiveable Customer Page",
+  title: "Agrimas - Payment Method Page",
   description:
     "Select a company to proceed based on your role. Streamline navigation and access specific company data with ease.",
   icons: [{ rel: "icon", url: "/favicon.ico" }],
 };
 const page = () => {
-  return <ReceiveablePage />;
+  return <HydrateClient>this is payment method page</HydrateClient>;
 };
 
 export default page;
