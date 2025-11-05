af9c63a7981bdae0b95af45c386ed0b202fc6bcd
--replace-all <kelvinhandoko03@gmail.com>
2025-11-05 14:40:47 +0700

    feat: improve discount validation logic in discountHandler function

--- src/utils/discountHandler.ts
+++ src/utils/discountHandler.ts
@@ -1,18 +1,35 @@
 import { TRPCError } from "@trpc/server";
 
 export const discountHandler = (totalBefore: number, discount: number) => {
+  if (totalBefore < 0) {
+    throw new TRPCError({
+      code: "BAD_REQUEST",
+      message: "total sebelum diskon tidak valid",
+    });
+  }
+
   if (discount === 0) {
     return 0;
   }
 
-  if (discount < 0 || discount > 100) {
+  if (discount < 0) {
     throw new TRPCError({
       code: "BAD_REQUEST",
       message: "diskon tidak valid",
     });
   }
 
-  const discountAmount = (totalBefore * discount) / 100;
+  if (discount <= 100) {
+    const discountAmount = (totalBefore * discount) / 100;
+    return discountAmount;
+  }
 
-  return discountAmount;
+  if (discount > totalBefore) {
+    throw new TRPCError({
+      code: "BAD_REQUEST",
+      message: "diskon nominal tidak boleh lebih besar dari total",
+    });
+  }
+
+  return discount;
 };
